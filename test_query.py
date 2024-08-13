c1916f4af81946feed412da804c5f699b7b01534
Vsevolod776 <ovchinnikov.seva@yandex.ru>
2024-08-13 09:35:02 +0300

    ref: мелки правки

--- test_query.py
+++ test_query.py
@@ -34,8 +34,9 @@ def createQueryForAdd(file_name, script_file, count, size):
         ]
         file.write("\n".join(ldif))
     file.close()
-    script_file.write("time ldbadd -H ldap://192.168.40.19 -U administrator --password=qqqwww12! " + file_name + "_add.ldif"+"\n")
-    print("\nCreate users file - done!")
+    script_file.write("{ time ldbadd -H ldap://192.168.40.19 -U administrator --password=qqqwww12! " + file_name + "_add.ldif } > result_add_data.txt\n")
+    script_file.write(f"echo \"{file_name} is done!\" > result_add_data.txt\n")
+    print(f"\nFile {file_name}_add.ldif - done!")
         
 #======================Delete======================#
 
@@ -48,9 +49,9 @@ def createQueryForDelete(file_name, script_file, count, size):
         ]
         file.write("\n".join(ldif))
     file.close()
-    script_file.write("time ldapdelete -H ldap://192.168.40.19 -x -D \"CN=administrator,CN=Users,DC=example,DC=tst\" -f " + file_name + "_delete.ldif -w qqqwww12! \n" )
-    print("\nFile for delete users - done!")
-
+    script_file.write("{ time ldapmodify -H ldap://192.168.40.19 -x -D \"CN=administrator,CN=Users,DC=example,DC=tst\" -f " + file_name + "_delete.ldif > /dev/null -w qqqwww12! } > result_delete_data.txt\n" )
+    script_file.write(f"echo \"{file_name} is done!\" > result_delete_data.txt\n")
+    print(f"\nFile {file_name}_delete.ldif - done!")
 
 #=======================Update====================#
 
@@ -65,28 +66,50 @@ def createQueryForUpdate(file_name, script_file, count, size):
         ]
         file.write("\n".join(ldif))
     file.close()
-    script_file.write("time ldapmodify -H ldap://192.168.40.19 -x -D \"CN=administrator,CN=Users,DC=example,DC=tst\" -f" + file_name + "_update.ldif -w qqqwww12! \n")
-    print("\nFile for delete users - done!")
+    script_file.write("{ time ldapmodify -H ldap://192.168.40.19 -x -D \"CN=administrator,CN=Users,DC=example,DC=tst\" -f " + file_name + "_update.ldif > /dev/null -w qqqwww12! } > result_update_data.txt\n")
+    script_file.write(f"echo \"{file_name} is done!\" > result_update_data.txt\n")
+    print(f"\nFile {file_name}_update.ldif - done!")
 
 #=================Script================#
 
-def createScriptFile( file, file_name ):
-
-
-    file.close()
-
-TestArray = [1000, 4000, 20000, 25000, 50000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000,
-             100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000,
-             100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000]
+TestArray = [1000, 4000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000,     #50k
+             10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, #150k
+             10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, #250k
+             10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, #350k
+             10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, #450k
+             10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, #550k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #700k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #850k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m150k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m300k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m450k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m600k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m750k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m900k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m50k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m200k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m350k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m500k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m650k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m800k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m950k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #3m100k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #3m250k
+             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #3m400k
+            ]
 
 init_value = 1
 
 script_add_file = open("add_script.sh", 'w')
-script_add_file.write("#!/bin/bash \n")
+script_add_file.write("#!/bin/bash\n")
+script_add_file.write("touch result_add_data.txt\n")
 script_upd_file = open("upd_script.sh", 'w')
-script_upd_file.write("#!/bin/bash \n")
+script_upd_file.write("#!/bin/bash\n")
+script_upd_file.write("touch result_update_data.txt\n")
 script_del_file = open("del_script.sh", 'w')
-script_del_file.write("#!/bin/bash \n")
+script_del_file.write("#!/bin/bash\n")
+script_del_file.write("touch result_delete_data.txt\n")
 
 for value in TestArray:
     fullName = FileName+"_"+str(init_value)+"-"+str(init_value+value-1)
