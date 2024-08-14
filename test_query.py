0cec8e9e3cc05d4d30b0636920a87074d77c3971
Vsevolod776 <ovchinnikov.seva@yandex.ru>
2024-08-14 12:56:47 +0300

    ref: Правки, правки, правки

--- test_query.py
+++ test_query.py
@@ -15,7 +15,7 @@ for a in Domain.split("."):
 
 def createQueryForAdd(file_name, script_file, count, size):
     file = open(file_name+"_add.ldif", 'w')
-    for i in range(count, count + size):
+    for i in range(count, size):
         ldif = [
             f"dn: CN=user_{i},{domain_dn}",
              "objectClass: top",
@@ -30,76 +30,47 @@ def createQueryForAdd(file_name, script_file, count, size):
             f"sAMAccountName: user_{i}",
              "userAccountControl: 512",
              f"mail: user_{i}@{Domain}"
-             "unicodePwd:: " + base64.b64encode(f'"{UserPass}"'.encode('UTF-16LE')).decode('UTF-8')
+             "unicodePwd:: " + base64.b64encode(f'"{UserPass}"'.encode('UTF-16LE')).decode('UTF-8'),
+             "\n"
         ]
         file.write("\n".join(ldif))
     file.close()
-    script_file.write("{ time ldbadd -H ldap://192.168.40.19 -U administrator --password=qqqwww12! " + file_name + "_add.ldif } > result_add_data.txt\n")
-    script_file.write(f"echo \"{file_name} is done!\" > result_add_data.txt\n")
-    print(f"\nFile {file_name}_add.ldif - done!")
+    script_file.write("{ time ldbadd -H ldap://192.168.40.19 -U administrator --password=qqqwww12! " + file_name + "_add.ldif; } 2>> result_add_data.txt\n")
+    script_file.write(f"echo \"{file_name} is done!\" >> result_add_data.txt\n")
         
 #======================Delete======================#
 
 def createQueryForDelete(file_name, script_file, count, size):
     file = open(file_name+"_delete.ldif", 'w')
-    for i in range(count, count + size):
+    for i in range(count, size):
         ldif = [
             f"dn: CN=user_{i},{domain_dn}",
-            "changetype: delete"
+            "changetype: delete",
+            "\n"
         ]
         file.write("\n".join(ldif))
     file.close()
-    script_file.write("{ time ldapmodify -H ldap://192.168.40.19 -x -D \"CN=administrator,CN=Users,DC=example,DC=tst\" -f " + file_name + "_delete.ldif > /dev/null -w qqqwww12! } > result_delete_data.txt\n" )
-    script_file.write(f"echo \"{file_name} is done!\" > result_delete_data.txt\n")
-    print(f"\nFile {file_name}_delete.ldif - done!")
+    script_file.write("{ time ldapmodify -H ldap://192.168.40.19 -x -D \"CN=administrator,CN=Users,DC=example,DC=tst\" -w qqqwww12! -f " + file_name + "_delete.ldif > /dev/null; } 2>> result_delete_data.txt\n" )
+    script_file.write(f"echo \"{file_name} is done!\" >> result_delete_data.txt\n")
 
 #=======================Update====================#
 
 def createQueryForUpdate(file_name, script_file, count, size):
     file = open(file_name+"_update.ldif", 'w')
-    for i in range(count, count + size):
+    for i in range(count, size):
         ldif = [
             f"dn: CN=user_{i},{domain_dn}",
             "changetype: modify",
             "replace: mail",
-            f"mail: user{i}@{Domain}.mod"
+            f"mail: user{i}@{Domain}.mod",
+            "\n"
         ]
         file.write("\n".join(ldif))
     file.close()
-    script_file.write("{ time ldapmodify -H ldap://192.168.40.19 -x -D \"CN=administrator,CN=Users,DC=example,DC=tst\" -f " + file_name + "_update.ldif > /dev/null -w qqqwww12! } > result_update_data.txt\n")
-    script_file.write(f"echo \"{file_name} is done!\" > result_update_data.txt\n")
-    print(f"\nFile {file_name}_update.ldif - done!")
+    script_file.write("{ time ldapmodify -H ldap://192.168.40.19 -x -D \"CN=administrator,CN=Users,DC=example,DC=tst\" -w qqqwww12! -f " + file_name + "_update.ldif > /dev/null; } 2>> result_update_data.txt\n")
+    script_file.write(f"echo \"{file_name} is done!\" >> result_update_data.txt\n")
 
 #=================Script================#
-
-TestArray = [1000, 4000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000,     #50k
-             10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, #150k
-             10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, #250k
-             10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, #350k
-             10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, #450k
-             10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, #550k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #700k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #850k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m150k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m300k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m450k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m600k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m750k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #1m900k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m50k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m200k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m350k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m500k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m650k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m800k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #2m950k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #3m100k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #3m250k
-             15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, 15000, #3m400k
-            ]
-
-init_value = 1
 
 script_add_file = open("add_script.sh", 'w')
 script_add_file.write("#!/bin/bash\n")
@@ -111,13 +82,19 @@ script_del_file = open("del_script.sh", 'w')
 script_del_file.write("#!/bin/bash\n")
 script_del_file.write("touch result_delete_data.txt\n")
 
-for value in TestArray:
-    fullName = FileName+"_"+str(init_value)+"-"+str(init_value+value-1)
-    createQueryForAdd( fullName, script_add_file, init_value, value )
-    createQueryForUpdate( fullName, script_upd_file, init_value, value )
-    createQueryForDelete( fullName, script_del_file, init_value, value )
-    init_value += value
+сount_record = int(input("Write count records:"))
+step = int(input("Write step:"))
 
+for current_value in range(1,сount_record, step):
+    fullName = FileName+"_"+str(current_value)+"-"+str(current_value+step-1 )
+    createQueryForAdd( fullName, script_add_file, current_value, current_value+step )
+    createQueryForUpdate( fullName, script_upd_file, current_value, current_value+step )
+    createQueryForDelete( fullName, script_del_file, current_value, current_value+step )
+
+print("LDAP requests have been created!")
+
+script_add_file.write("nohup ./upd_script.sh\nexit")
 script_add_file.close()
+script_upd_file.write("nohup ./del_script.sh\nexit")
 script_upd_file.close()
 script_del_file.close()
