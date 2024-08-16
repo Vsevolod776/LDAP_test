ddd7a3ef97b8ba93138b2094ea661f9e1f610832
Vsevolod776 <ovchinnikov.seva@yandex.ru>
2024-08-16 09:03:42 +0300

    ref: Поправил алгоритм тестирования

--- test_query.py
+++ test_query.py
@@ -1,5 +1,5 @@
 #===========CHANGE==========================================
-Domain    = "example.tst"   # Именование домена            =
+Domain    = "test-example.tst"   # Именование домена            =
 UserPref  = "user_"         # Префикс пользователя         =
 UserPass  = "qqqwww12!"     # Пароль пользователей         =
 FileName  = "users"         # Имя файла                    =
@@ -11,10 +11,52 @@ domain_dn = "CN=Users"
 for a in Domain.split("."):
     domain_dn += f",DC={a}"
 
-#======================Create======================#
+#=======================Update====================#
 
+def queryModifyAfterAdd(file_name, script_file, count, size):
+    file = open(file_name+"_update.ldif", 'w')
+    for i in range(count, size):
+        ldif = [
+            f"dn: CN=user_{i},{domain_dn}",
+            "changetype: modify",
+            "replace: mail",
+            f"mail: user{i}@{Domain}.mod",
+            "\n"
+        ]
+        file.write("\n".join(ldif))
+    file.close()
+
+def queryModifyBeforeDelete(file_name, script_file, count, size):
+    file = open(file_name+"_update.ldif", 'w')
+    for i in range(count, size):
+        ldif = [
+            f"dn: CN=user_{i},{domain_dn}",
+            "changetype: modify",
+            "replace: mail",
+            f"mail: Yser{i}@{Domain}.mod",
+            "\n"
+        ]
+        file.write("\n".join(ldif))
+    file.close()
+
+#======================Read======================#
+
+def queryRead(file_name, script_file, count, size):
+    file = open(file_name+"_update.ldif", 'w')
+    for i in range(count, size):
+        ldif = [
+            f"dn: {domain_dn}",
+            "uid: user_{i}",
+            "\n"
+        ]
+        file.write("\n".join(ldif))
+    file.close()
+
+#======================Create======================#
+#ldapsearch -H ldap://192.168.40.20 -x -D "CN=administrator,CN=Users,DC=test-example,DC=tst" -w qqqwww12! -b "dc=test-example,dc=tst" "(cn=user_1)"
 def createQueryForAdd(file_name, script_file, count, size):
     file = open(file_name+"_add.ldif", 'w')
+    readQuery = open(file_name + "_read_after_add.sh", 'w')
     for i in range(count, size):
         ldif = [
             f"dn: CN=user_{i},{domain_dn}",
@@ -34,14 +76,24 @@ def createQueryForAdd(file_name, script_file, count, size):
              "\n"
         ]
         file.write("\n".join(ldif))
+        readQuery.write(f"ldapsearch -H ldap://192.168.40.20 -x -D \"CN=administrator,CN=Users,DC=test-example,DC=tst\" -w qqqwww12! -b \"dc=test-example,dc=tst\" \"(cn=user_{i})\"\n")
     file.close()
-    script_file.write("{ time ldbadd -H ldap://192.168.40.19 -U administrator --password=qqqwww12! " + file_name + "_add.ldif; } 2>> result_add_data.txt\n")
-    script_file.write(f"echo \"{file_name} is done!\" >> result_add_data.txt\n")
+    #add
+    script_file.write("{ time ldbadd -H ldap://192.168.40.20 -U administrator --password=qqqwww12! " + file_name + "_add.ldif; } 2>> result_add_data.txt | tr '\\n' ' '\n")
+    script_file.write(f"echo \"{file_name} is done!\" >> result_add.txt\n")
+    #read
+    script_file.write( f"chmod +x {file_name}_read_after_add.sh\ntime ./{file_name}_read_after_add.sh >> result_add_read.txt\n")
+    script_file.write(f"echo \"{file_name} is done!\" >> result_add_read.txt\n")
+    #update
+    queryModifyAfterAdd(file_name, script_file, count, size)
+    script_file.write("{ time ldapmodify -H ldap://192.168.40.20 -x -D \"CN=administrator,CN=Users,DC=example,DC=tst\" -w qqqwww12! -f " + file_name + "_update.ldif > /dev/null; } 2>> result_update_data.txt | tr '\\n' ' '\n")
+    script_file.write(f"echo \"{file_name} is done!\" >> result_update.txt\n")
         
 #======================Delete======================#
 
 def createQueryForDelete(file_name, script_file, count, size):
     file = open(file_name+"_delete.ldif", 'w')
+    readQuery = open(file_name + "_read_before_delete.sh", 'w')
     for i in range(count, size):
         ldif = [
             f"dn: CN=user_{i},{domain_dn}",
@@ -49,38 +101,32 @@ def createQueryForDelete(file_name, script_file, count, size):
             "\n"
         ]
         file.write("\n".join(ldif))
+        readQuery.write(f"ldapsearch -H ldap://192.168.40.20 -x -D \"CN=administrator,CN=Users,DC=test-example,DC=tst\" -w qqqwww12! -b \"dc=test-example,dc=tst\" \"(cn=user_{i})\"\n")
     file.close()
-    script_file.write("{ time ldapmodify -H ldap://192.168.40.19 -x -D \"CN=administrator,CN=Users,DC=example,DC=tst\" -w qqqwww12! -f " + file_name + "_delete.ldif > /dev/null; } 2>> result_delete_data.txt\n" )
-    script_file.write(f"echo \"{file_name} is done!\" >> result_delete_data.txt\n")
+    #update
+    queryModifyBeforeDelete(file_name, script_file, count, size)
+    script_file.write("{ time ldapmodify -H ldap://192.168.40.20 -x -D \"CN=administrator,CN=Users,DC=example,DC=tst\" -w qqqwww12! -f " + file_name + "_update.ldif > /dev/null; } 2>> result_update_data.txt | tr '\\n' ' '\n")
+    script_file.write(f"echo \"{file_name} is done!\" >> result_update.txt\n")
+    #read
+    script_file.write(f"chmod +x {file_name}_read_before_delete.sh\ntime ./{file_name}_read_before_delete.sh >> result_del_read.txt\n")
+    script_file.write(f"echo \"{file_name} is done!\" >> result_del_read.txt\n")
+    #delete
+    script_file.write("{ time ldapmodify -H ldap://192.168.40.20 -x -D \"CN=administrator,CN=Users,DC=example,DC=tst\" -w qqqwww12! -f " + file_name + "_delete.ldif > /dev/null; } 2>> result_delete_data.txt | tr '\\n' ' '\n" )
+    script_file.write(f"echo \"{file_name} is done!\" >> result_delete.txt\n")
 
-#=======================Update====================#
-
-def createQueryForUpdate(file_name, script_file, count, size):
-    file = open(file_name+"_update.ldif", 'w')
-    for i in range(count, size):
-        ldif = [
-            f"dn: CN=user_{i},{domain_dn}",
-            "changetype: modify",
-            "replace: mail",
-            f"mail: user{i}@{Domain}.mod",
-            "\n"
-        ]
-        file.write("\n".join(ldif))
-    file.close()
-    script_file.write("{ time ldapmodify -H ldap://192.168.40.19 -x -D \"CN=administrator,CN=Users,DC=example,DC=tst\" -w qqqwww12! -f " + file_name + "_update.ldif > /dev/null; } 2>> result_update_data.txt\n")
-    script_file.write(f"echo \"{file_name} is done!\" >> result_update_data.txt\n")
 
 #=================Script================#
-
-script_add_file = open("add_script.sh", 'w')
+script_add_file = open("add.sh", 'w')
 script_add_file.write("#!/bin/bash\n")
-script_add_file.write("touch result_add_data.txt\n")
-script_upd_file = open("upd_script.sh", 'w')
-script_upd_file.write("#!/bin/bash\n")
-script_upd_file.write("touch result_update_data.txt\n")
-script_del_file = open("del_script.sh", 'w')
+script_add_file.write("touch result_add.txt\n")
+script_add_file.write("touch result_add_read.txt\n")
+script_add_file.write("touch result_update.txt\n")
+#=====================================================#
+script_del_file = open("delete.sh", 'w')
 script_del_file.write("#!/bin/bash\n")
-script_del_file.write("touch result_delete_data.txt\n")
+script_del_file.write("touch result_update.txt\n")
+script_del_file.write("touch result_del_read.txt\n")
+script_del_file.write("touch result_delete.txt\n")
 
 сount_record = int(input("Write count records:"))
 step = int(input("Write step:"))
@@ -88,13 +134,10 @@ step = int(input("Write step:"))
 for current_value in range(1,сount_record, step):
     fullName = FileName+"_"+str(current_value)+"-"+str(current_value+step-1 )
     createQueryForAdd( fullName, script_add_file, current_value, current_value+step )
-    createQueryForUpdate( fullName, script_upd_file, current_value, current_value+step )
     createQueryForDelete( fullName, script_del_file, current_value, current_value+step )
 
 print("LDAP requests have been created!")
 
-script_add_file.write("nohup ./upd_script.sh\nexit")
+script_add_file.write("nohup ./delete.sh\nexit")
 script_add_file.close()
-script_upd_file.write("nohup ./del_script.sh\nexit")
-script_upd_file.close()
 script_del_file.close()
