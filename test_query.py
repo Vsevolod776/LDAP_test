355fa7c25c6b2b835aa1df473cc3cfeecca43817
Vsevolod776 <ovchinnikov.seva@yandex.ru>
2024-08-16 15:11:13 +0300

    ref: лишняя запись в текстовик

--- test_query.py
+++ test_query.py
@@ -109,16 +109,15 @@ def createQueryForDelete(file_name, script_file, count, size):
     file.close()
     #update
     queryModifyBeforeDelete(file_name, script_file, count, size)
-    script_file.write(f"{{ time ldapmodify -H ldap://{ip} -x -D \"CN=administrator,CN=Users,DC={DC1},DC={DC2}\" -w qqqwww12! -f " + file_name + "_del_update.ldif > /dev/null; } 2>> result_delete_update.txt | tr '\\n' ' '\n")
-    script_file.write(f"echo \"{file_name} is done!\" >> result_delete_update.txt\n")
+    script_file.write( f"{{ time ldapmodify -H ldap://{ip} -x -D \"CN=administrator,CN=Users,DC={DC1},DC={DC2}\" -w qqqwww12! -f " + file_name + "_del_update.ldif > /dev/null; } 2>> result_delete_update.txt | tr '\\n' ' '\n")
+    script_file.write( f"echo \"{file_name} is done!\" >> result_delete_update.txt\n")
     #read
-    script_file.write(f"chmod +x {file_name}_read_before_delete.sh\ntime ./{file_name}_read_before_delete.sh >> result_delete_read.txt\n")
-    script_file.write(f"{{ time ./{file_name}_read_before_delete.sh >/dev/null; }} 2>> result_delete_read.txt\n")
-    script_file.write(f"echo \"{file_name} is done!\" >> result_delete_read.txt\n")
+    script_file.write( f"chmod +x {file_name}_read_before_delete.sh\n")
+    script_file.write( f"{{ time ./{file_name}_read_before_delete.sh >/dev/null; }} 2>> result_delete_read.txt\n")
+    script_file.write( f"echo \"{file_name} is done!\" >> result_delete_read.txt\n")
     #delete
     script_file.write(f"{{ time ldapmodify -H ldap://{ip} -x -D \"CN=administrator,CN=Users,DC={DC1},DC={DC2}\" -w qqqwww12! -f " + file_name + "_delete.ldif > /dev/null; } 2>> result_delete.txt | tr '\\n' ' '\n" )
     script_file.write(f"echo \"{file_name} is done!\" >> result_delete.txt\n\n")
-
 
 #=================Script================#
 script_add_file = open("add.sh", 'w')
